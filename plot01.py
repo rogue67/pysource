82dc8abd0498e399472d18a85e7dbd1be2590a05
Roger Forsman <Roger.Forsman.6455@student.uu.se>
2022-04-26 17:50:39 +0200

    Small test plot

---
+++ plot01.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 19 14:44:21 2022
+
+@author: rofo6455
+"""
+
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
